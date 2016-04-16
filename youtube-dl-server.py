94c80a820fa73d04595ab0f7220cc341cbce4c8f
Antoni Kaniowski <a@generativestuff.com>
2016-04-16 17:16:19 +0200

    add support for http basic and configurable path

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -2,24 +2,40 @@ import json
 import os
 import subprocess
 from queue import Queue
-from bottle import route, run, Bottle, request, static_file
+from bottle import route, run, Bottle, request, static_file, auth_basic
 from threading import Thread
 
 app = Bottle()
+user = os.environ['YTDL_USER']
+password = os.environ['YTDL_PASS']
+path = os.environ['YTDL_PATH'].rstrip('//')
+port = os.getenv('YTDL_PORT', 8080)
 
-@app.route('/youtube-dl')
+def check(u, p):
+    if (u == user) and (p == password):
+        return True
+    else:
+        return False
+
+
+@app.route('/ytdl')
+@auth_basic(check)
 def dl_queue_list():
     return static_file('index.html', root='./')
 
-@app.route('/youtube-dl/static/:filename#.*#')
-def server_static(filename):
-    return static_file(filename, root='./static')
 
-@app.route('/youtube-dl/q', method='GET')
+@app.route('/ytdl/static/:filename#.*#')
+@auth_basic(check)
+def server_static(filename):
+    return static_file(filename, root='static/')
+
+@app.route('/ytdl/q', method='GET')
+@auth_basic(check)
 def q_size():
     return { "success" : True, "size" : json.dumps(list(dl_q.queue)) }
 
-@app.route('/youtube-dl/q', method='POST')
+@app.route('/ytdl/q', method='POST')
+@auth_basic(check)
 def q_put():
     url = request.forms.get( "url" )
     if "" != url:
@@ -37,7 +53,7 @@ def dl_worker():
 
 def download(url):
     print("Starting download of " + url)
-    command = """youtube-dl -o "/youtube-dl/.incomplete/%(title)s.%(ext)s" -f best[acodec=none][ext=mp4]+best[vcodec=none][ext=m4a] --exec 'touch {} && mv {} /youtube-dl/' --merge-output-format mp4 """ + url
+    command = 'youtube-dl -o "' + path + '/.incomplete/%(title)s.%(ext)s" -f best[acodec=none][ext=mp4]+best[vcodec=none][ext=m4a] --exec \'touch {} && mv {} ' + path + '\' --merge-output-format mp4 ' + url
     subprocess.call(command, shell=True)
     print("Finished downloading " + url)
 
@@ -48,6 +64,6 @@ dl_thread.start()
 
 print("Started download thread")
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=port, debug=True)
 done = True
 dl_thread.join()
